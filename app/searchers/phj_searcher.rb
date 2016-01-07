b12531455c50bcf50c889d3e011070764239ca08
Frederich Chen <frederichchen@hotmail.com>
2016-01-07 16:55:49 +0800

    Bug Fixes

--- app/searchers/phj_searcher.rb
+++ app/searchers/phj_searcher.rb
@@ -6,7 +6,7 @@ class PHJSearcher
   attr_reader :stmt, :results
 
   def initialize(sv)
-    @stmt = "select distinct * from (select 姓名, 身份证号, 户号, 与户主关系 from hjinfoandczgy where 户号 in (select 户号 from hjinfoandczgy where 身份证号='?') and 与户主关系<>'f非亲属') a order by 户号"
+    @stmt = "select distinct * from (select 姓名, 身份证号, 户号, 与户主关系 from hjinfoandczgy where 户号 in (select 户号 from hjinfoandczgy where 身份证号='?') and 与户主关系<>'非亲属') a order by 户号"
     @stmt.gsub!(/\?/, sv)
   end
 
